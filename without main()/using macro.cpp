4fa402f0be63b5b2b28d63e757f315d0111d54bf
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-05-05 20:34:13 +0530

    Add files via upload

---
+++ without main()/using macro.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#define fun main
+int fun(void)
+{
+	printf("Hello main");
+	return 0;
+}
---
+++ without main()/using token pasting operator.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define fun m##a##i##n
+int fun(){
+printf("hello mains");
+return 0;
+}
